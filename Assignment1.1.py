c41f004376782411dda0af62a38ba03cc37d6496
Ramaraju <buddharajuramaraju@gmail.com>
2018-01-28 20:47:08 +0530

    Assignment1.1

---
+++ Assignment1.1.py
@@ -0,0 +1,24 @@
+
+# coding: utf-8
+
+# ### Problem Statement
+# 
+# Install Jupyter notebook and run the first program and share the screenshot of the output.
+
+# In[2]:
+
+
+import platform
+
+
+# In[20]:
+
+
+platform.python_version()
+
+
+# In[21]:
+
+
+print ("This is my first program")
+
